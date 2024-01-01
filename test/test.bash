0fb6e4c0a7db098b25b6f08e6cb235048a421ac1
Nishi Touki <touki2424@gmail.com>
2024-01-01 16:30:08 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -10,9 +10,9 @@ colcon build
 
 source $dir/.bashrc
 
-timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep '6月の誕生石: moonstone' 
+grep '待機中' 
 
 
