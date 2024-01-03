00412044378f526c0303bd280a178e16a4d282f1
Nishi Touki <touki2424@gmail.com>
2024-01-03 16:45:20 +0900

    2

--- test/test.bash
+++ test/test.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
-#SPDX-FileCopyrightText: 2024 Touki Nishi
-#SPDX-Lisense^Identifire: BSD-3-Clause
 
 dir=~
-[ "$1" != "" ] && dir="$1"  
+[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
 cd $dir/ros2_ws
 colcon build
@@ -13,8 +11,4 @@ source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep '11月の誕生石: topaz' 
-
-
-
-
+grep '6月の誕生石' 
