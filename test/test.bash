a5235f20629e553cf2c13e81b2fc4161331f666a
Nishi Touki <touki2424@gmail.com>
2024-01-04 17:39:33 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -2,11 +2,16 @@
 
 dir=~
 [ "$1" != "" ] && dir="$1"  
+source $dir/.bashrc
 cd $dir/ros2_ws/src
 git clone https://github.com/pl1-NT/person_msgs.git
+
 cd $dir/ros2_ws
 colcon build
+source $dir/.bashrc
 ros2 interface show "person_msgs/srv/Query2"
+cd $dir/ros2_ws
+colcon build
 source $dir/.bashrc
 
 timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
