7038f919d74bf51cc375cf5881843e8ac4f52c59
Nishi Touki <touki2424@gmail.com>
2024-01-04 16:28:16 +0900

    commit

--- test/test.bash
+++ test/test.bash
@@ -2,7 +2,6 @@
 
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
-
 cd $dir/ros2_ws
 colcon build
 
@@ -11,4 +10,4 @@ source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep 'Listen: 10' 
+grep 'listen:' 
