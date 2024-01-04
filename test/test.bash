cf1e491b7d266ebc32f6465eda9729ec00ac5cfb
Nishi Touki <touki2424@gmail.com>
2024-01-04 16:50:25 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 dir=~
-[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
+[ "$1" != "" ] && dir="$1"  
 cd $dir/ros2_ws/src
 git clone https://github.com/pl1-NT/person_msgs.git
 cd $dir/ros2_ws
 colcon build
-
+ros2 interface show "person_msgs/srv/Query2"
 source $dir/.bashrc
 
 timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
